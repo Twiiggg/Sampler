68096fd0619cc1b21351c0c8aa4c8786c5c0e17e
Twiiggg <odias001@gmail.com>
2025-06-26 01:36:14 -0300

    links para tela de usuario

--- home/script.js
+++ home/script.js
@@ -53,6 +53,33 @@ function scrollSlider2(direction) {
 
   requestAnimationFrame(animateScroll);
 }
+function scrollSlider3(direction) {
+  const slider = document.getElementById('cardSlider3');
+  const cardWidth = slider.querySelector('.card2').offsetWidth + 16;
+  const scrollAmount = cardWidth * 1;
+  const start = slider.scrollLeft;
+  const end = start + scrollAmount * direction;
+  const duration = 500; // milissegundos (mais alto = mais lento)
+  const startTime = performance.now();
+
+  function animateScroll(currentTime) {
+    const elapsed = currentTime - startTime;
+    const progress = Math.min(elapsed / duration, 1);
+    slider.scrollLeft = start + (end - start) * easeInOutCubic(progress);
+
+    if (progress < 1) {
+      requestAnimationFrame(animateScroll);
+    }
+  }
+
+  function easeInOutCubic(t) {
+    return t < 0.5
+      ? 4 * t * t * t
+      : 1 - Math.pow(-2 * t + 2, 3) / 2;
+  }
+
+  requestAnimationFrame(animateScroll);
+}
 
 /* PUBLICAR */
 
