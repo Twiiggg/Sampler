3243a0068385d6c2a4417e4720f28c8d46949376
Josué <josue.lima@etec.sp.gov.br>
2025-09-09 22:35:08 -0300

    corrigindo problemas no design do form

--- auth/script.js
+++ auth/script.js
@@ -1,11 +1,11 @@
 const goToSignup = document.getElementById('goToSignup');
 const goToSignin = document.getElementById('goToSignin');
-const container = document.getElementById('container');
+const container = document.querySelector('.container');
 
 goToSignup.addEventListener('click', () => {
-    container.classList.toggle('change');
+    container.classList.add('change');
 })
 
 goToSignin.addEventListener('click', () => {
-    container.classList.toggle('change');
+    container.classList.remove('change');
 })
